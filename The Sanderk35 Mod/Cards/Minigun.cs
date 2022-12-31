2a81dbbf05a3d094bdc010e74c58ef42058f2d05
Sanderk35 <sanderkamphuis719@gmail.com>
2022-12-31 12:54:40 +0100

    My mod

---
+++ The Sanderk35 Mod/Cards/Minigun.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace TheSanderk35Mod.Cards
+{
+    class Minigun : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            gun.damage = 0.5f;
+            gun.projectileSpeed = 6.5f;
+            gun.ammo = 40;
+            gun.reloadTimeAdd = 1.5f;
+            gun.attackSpeed = -0.25f;
+            gun.dontAllowAutoFire = false;
+            gun.knockback = 1.5f;
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Minigun";
+        }
+        protected override string GetDescription()
+        {
+            return "Gives you like infinite amount of ammo";
+        }
+        public override string GetModName()
+        {
+            return "TSM";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Bullets",
+                    amount = "40",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "DMG",
+                    amount = "-50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Bullet speed",
+                    amount = "+50",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Reload speed",
+                    amount = "0",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+    }
+}
---
+++ The Sanderk35 Mod/TheSanderk35Mod.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using TheSanderk35Mod.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace TheSanderk35Mod
+{
+    // These are the mods required for our Mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our Mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our Mod Is associated with
+    [BepInProcess("Rounds.exe")]
+    public class TutorialMod :  BaseUnityPlugin
+    {
+        private const string ModId = "com.My.Mod.Id";
+        private const string ModName = "The Sanderk35 Mod";
+        public const string Version = "0.1.0"; // What version are we On (major.minor.patch)?
+        public const string ModInitials = "TSM";
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your Mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<Minigun>();
+        }
+    }
+}
